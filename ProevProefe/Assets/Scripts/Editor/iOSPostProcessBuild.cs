4ba1255ef59b591edf285f89d8e929213b40504d
burhanos2 <burhantopaloglu60@gmail.com>
2021-03-07 14:58:45 +0100

    added iOS build support and imported textmesh pro
    imported textmeshpro, added a postprocessbuild file. the contents of the permission message need to be added, for now its just filler

---
+++ ProevProefe/Assets/Scripts/Editor/iOSPostProcessBuild.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+
+// this class is required to add "NSMotionUsageDescription" key to info plist upon building, this determines the message sent to the user when requesting pedometer permissions
+
+public class iOSPostProcessBuild : MonoBehaviour
+{
+    [PostProcessBuild]
+    public static void ChangeXCodePlist(BuildTarget buildTarget, string pathToBuiltProject)
+    {
+        const string motionUsageDescription = "NSMotionUsageDescription";
+        if(buildTarget == BuildTarget.iOS)
+        {
+            string plistPath = pathToBuiltProject + "/Info.plist";
+            PlistDocument plist = new PlistDocument();
+            plist.ReadFromFile(plistPath);
+            PlistElementDict root = plist.root;
+
+            PlistElementArray arr = new PlistElementArray();
+            arr.AddString(PermissionMessages.GetMessageInDeviceLanguage());
+            root[motionUsageDescription] = arr;
+
+            plist.WriteToFile(plistPath);
+        }
+    }
+
+}

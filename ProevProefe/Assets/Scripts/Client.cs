7ed0a20ebfacc57b30550460685242b177887ff7
DragstylerNL <dragstylernl@gmail.com>
2021-03-09 14:13:33 +0100

    server connection enabled and active.

---
+++ ProevProefe/Assets/Scripts/Client.cs
@@ -0,0 +1,34 @@
+﻿using SocketIO;
+
+public class Client : SocketIOComponent
+{
+    public override void Start()
+    {
+        base.Start();
+        SetupEvents();
+    }
+    
+    public override void Update()
+    {
+        base.Update();
+    }
+
+    private void SetupEvents()
+    {
+        // RECIEVING
+        On("open", (data) => // -------------- when connected
+        {
+            print("Connection made to server ^^");
+        });
+        
+        On("leaderboard", (data) => // -------------- when connected
+        {
+            print("LeaderBoard incoming");
+        });
+    }
+
+    public void PostScore(string name, int score)
+    {
+        //Emit("newscore", );
+    }
+}

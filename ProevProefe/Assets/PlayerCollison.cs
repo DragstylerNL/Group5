9289dc46c346b6f8fb79f0cd673c8be3c6ab6ff3
PieterWinderlichSchool <pieterwinderlich@outlook.com>
2021-03-09 15:15:29 +0100

    Added Collison check
    Added collision check and modified main Scene by adding collision + player input

---
+++ ProevProefe/Assets/PlayerCollison.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollison : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.tag == "Obstruction")
+        {
+            Debug.Log("Life lost");
+        }
+    }
+}
